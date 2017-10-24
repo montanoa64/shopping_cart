b2f569fe7562f5228f1a6739bada58d39d30eb4c
montanoa64 <andmontano89@gmail.com>
2017-10-23 23:29:56 -0500

    Added express, express-handlebars

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', (req, res, next) => {
+  res.render('index', { 
+    title: 'Express',
+    message: 'Hello World!!!!'
+   });
+});
+
+module.exports = router;
